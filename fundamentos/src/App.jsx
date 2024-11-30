2facc16028a12fa438c1cd202235f097a2d0100b
Ramon Pacheco <rmpacheco2@gmail.com>
2024-11-30 19:04:26 -0300

    numero aleatorio

--- fundamentos/src/App.jsx
+++ fundamentos/src/App.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from "./components/basicos/Aleatorio";
 
 const app = () => (
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Aleatorio min={10} max={30} />
         <Fragmento />
         <ComParametro
             titulo="Segundo componente"
