a4022a2c686055d76cffaedd7e2003a83640483e
Ramon Pacheco <rmpacheco2@gmail.com>
2024-11-30 18:09:27 -0300

    compomente com propriedade

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro
+            titulo="Segundo componente"
+            subtitulo="legal" />
     </div>,
     document.getElementById('root')
 )
