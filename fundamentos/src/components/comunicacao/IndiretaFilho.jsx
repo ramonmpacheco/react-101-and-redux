8c596cac468ca2d233c4eeb7e0a6f751bbb61f53
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-03 18:49:29 -0300

    componente com estado

--- fundamentos/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const IndiretaFilho = (props) => {
+
+    const gerarIdade = () => parseInt(Math.random() * (20)) + 50
+    const gerarNerd = () => Math.random() > 0.5
+
     return (
         <div>
             <div>Filho</div>
-            <button onClick={_ => props.quandoClicar("X", 10, false)}>
+            <button onClick={_ => props.quandoClicar("X", gerarIdade, gerarNerd)}>
                 Fornecer Informações
             </button>
         </div>
--- fundamentos/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos/src/components/comunicacao/IndiretaPai.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 const IndiretaPai = (props) => {
+    const [nome, setNome] = useState("?")
+    const [idade, setIdade] = useState(0)
+    const [nerd, setNerd] = useState(false)
+
     function fornecerInformacoes(nome, idade, nerd) {
-        console.log(nome, idade, nerd);
+        setNome(nome)
+        setIdade(idade)
+        setNerd(nerd)
     }
     return (
         <div>
-            <div>Pai</div>
+            <div>
+                <span>{nome} </span>
+                <span><strong>{idade}</strong></span>
+                <span> {nerd ? 'Verdadeiro' : 'Falso'}</span>
+            </div>
             <IndiretaFilho quandoClicar={fornecerInformacoes} />
         </div>
     )
