00ec4794fb48a46c0a14e39f1e633e3b3eb60677
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-09 07:08:50 -0300

    componente calculator

---
+++ calculadora/src/main/Calculadora.jsx
@@ -0,0 +1,14 @@
+import './Calculadora.css'
+import React, { Component } from "react";
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div className='calculator'>
+
+            </div>
+        )
+    }
+}
+
+export default Calculator
