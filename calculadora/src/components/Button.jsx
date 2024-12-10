14077d67659be841b03585bd46145b0cf3b508f3
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-10 06:59:58 -0300

    componente button 1

---
+++ calculadora/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import './Button.css'
+import React from "react";
+
+const Button = (props) => {
+    return (
+        <button className='button'>{props.label}</button>
+    )
+} 
+
+export default Button
--- calculadora/src/main/Calculadora.jsx
+++ calculadora/src/main/Calculadora.jsx
@@ -1,3 +1,4 @@
+import Button from '../components/Button';
 import './Calculadora.css'
 import React, { Component } from "react";
 
@@ -5,7 +6,23 @@ class Calculator extends Component {
     render() {
         return (
             <div className='calculator'>
-
+                <Button label="AC"/>
+                <Button label="/"/>
+                <Button label="7"/>
+                <Button label="8"/>
+                <Button label="9"/>
+                <Button label="*"/>
+                <Button label="4"/>
+                <Button label="5"/>
+                <Button label="6"/>
+                <Button label="-"/>
+                <Button label="1"/>
+                <Button label="2"/>
+                <Button label="3"/>
+                <Button label="+"/>
+                <Button label="0"/>
+                <Button label="."/>
+                <Button label="="/>
             </div>
         )
     }
