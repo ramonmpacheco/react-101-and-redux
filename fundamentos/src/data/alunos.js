3758e1975a1b7d3fffa0fe58c16f739a3597a804
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-11 06:56:28 -0300

    remove unecessary code

--- fundamentos/src/data/alunos.js
+++
@@ -1,7 +0,0 @@
-export default [
-    { id: 1, nome: 'Ana', nota: 9.7 },
-    { id: 2, nome: 'Bia', nota: 7.5 },
-    { id: 3, nome: 'Daniel', nota: 5.3 },
-    { id: 4, nome: 'Paulo', nota: 8.2 },
-    { id: 5, nome: 'Sergio', nota: 6.1 }
-]
