a3a90184eb77a42fd86c73bdd813d4de6b84beda
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-14 08:57:26 -0300

    implementando logica 2

--- calculadora/src/main/Calculadora.jsx
+++ calculadora/src/main/Calculadora.jsx
@@ -17,7 +17,26 @@ class Calculator extends Component {
         this.setState({ ...initialState })
     }
     setOperation(operation) {
-        console.log(operation);
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+            return
+        }
+        const equals = operation === '='
+        const currentOperation = this.state.operation
+        const values = [...this.state.values]
+        try {
+            values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+        } catch (e) {
+            values[0] = this.state.values[0]
+        }
+        values[1] = 0
+        this.setState({
+            displayValue: values[0],
+            operation: equals ? null : operation,
+            current: equals ? 0 : 1,
+            clearDisplay: !equals,
+            values
+        })
     }
     addDigit(d) {
         if (d === '.' && this.state.displayValue.includes('.')) {
