5a21077a243fd6617aa81ba1422978cd630083cc
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-17 18:59:03 -0300

    estrutura do projeto

---
+++ navegacao/src/views/examples/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = (props) => {
+    return (
+        <div className="Home">
+            <h1>
+                Início
+            </h1>
+            <p>Bem Vindo.</p>
+        </div>
+    )
+}
+
+export default Home
