cd2b24d70fa0a950dc4f630641a0f78f901b55dd
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-28 07:43:35 -0300

    add use context 1

--- hooks/src/main.jsx
+++ hooks/src/main.jsx
@@ -1,14 +1,3 @@
-// import './index.css'
-// import ReactDOM from 'react-dom'
-// import React from 'react'
-
-// import App from './views/App'
-
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('root')
-// )
-
 import './main.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -6,14 +6,20 @@ import { BrowserRouter } from 'react-router-dom'
 import Menu from '../components/layout/Menu';
 import Content from '../components/layout/Content'
 
+import DataContext, {data} from '../data/DataContext';
+import { useState } from 'react';
+
 const App = (props) => {
+    const [state, setState] = useState(data)
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Menu />
-                <Content />
-            </BrowserRouter>
-        </div>
+        <DataContext.Provider value={{state, setState}}>
+            <div className="App">
+                <BrowserRouter>
+                    <Menu />
+                    <Content />
+                </BrowserRouter>
+            </div>
+        </DataContext.Provider >
     )
 }
 
