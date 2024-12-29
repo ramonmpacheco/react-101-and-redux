363eb402586a50af4eb52307c0ec1a3de25da18c
Ramon Pacheco <rmpacheco2@gmail.com>
2024-12-29 15:38:11 -0300

    add use reducer exec

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -11,6 +11,14 @@ function reducer(state, action) {
     switch (action.type) {
         case 'numberAdd2':
             return { ...state, number: state.number + 2 }
+        case 'numberMilti7':
+            return { ...state, number: state.number * 7 }
+        case 'numberDiv25':
+            return { ...state, number: state.number / 25 }
+        case 'numberInt':
+            return { ...state, number: parseInt(state.number) }
+        case 'numberAddN':
+            return { ...state, number: state.number + action.payload.n }
         case 'login':
             return { ...state, user: { name: action.payload.name } }
         default:
@@ -32,8 +40,13 @@ const UseReducer = (props) => {
                     : <span className="text">Sem Usuário</span>}
                 <span className="text">{state.number}</span>
                 <div>
-                    <button className="btn" onClick={() => dispatch({ type: 'login', payload: {name: 'João'} })}>Login</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'login', payload: { name: 'João' } })}>Login</button>
                     <button className="btn" onClick={() => dispatch({ type: 'numberAdd2' })}>+2</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberMilti7' })}>*7</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberDiv25' })}>/25</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberInt' })}>Int</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberAddN', payload: {n: -9} })}>-9</button>
+                    <button className="btn" onClick={() => dispatch({ type: 'numberAddN', payload: {n: 11} })}>11</button>
                 </div>
             </div>
         </div>
