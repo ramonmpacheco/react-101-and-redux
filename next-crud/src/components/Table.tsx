b2f1cc6dfe615be42beb27456bcd81792ff0817a
Ramon Pacheco <rmpacheco2@gmail.com>
2025-01-10 06:42:18 -0300

    add table 2

--- next-crud/src/components/Table.tsx
+++ next-crud/src/components/Table.tsx
@@ -8,26 +8,31 @@ const Table = (props: TableProps) => {
     function renderHeader() {
         return (
             <tr>
-                <th>Code</th>
-                <th>Name</th>
-                <th>Age</th>
+                <th className="text-left p-4">Code</th>
+                <th className="text-left p-4">Name</th>
+                <th className="text-left p-4">Age</th>
             </tr>
         )
     }
     function renderData() {
         return (
             props.clients?.map((c, i) => {
-                return <tr key={c.getId()}>
-                    <td>{c.getId()}</td>
-                    <td>{c.getName()}</td>
-                    <td>{c.getAge()}</td>
+                return <tr key={c.getId()}
+                    className={`${i % 2 === 0 ? 'bg-purple-100' : 'bg-purple-50'}`}
+                >
+                    <td className="text-left p-4">{c.getId()}</td>
+                    <td className="text-left p-4">{c.getName()}</td>
+                    <td className="text-left p-4">{c.getAge()}</td>
                 </tr>
             })
         )
     }
     return (
-        <table>
-            <thead>
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+                text-gray-100
+                bg-gradient-to-r from-purple-500 to-purple-800
+            `}>
                 {renderHeader()}
             </thead>
             <tbody>
