8b551c570fa4da7d5ec24297489c760751e2f15f
Ramon Pacheco <rmpacheco2@gmail.com>
2025-01-21 07:05:03 -0300

    add db config

---
+++ todo-app/backend/src/config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+module.exports = mongoose.connect('mongodb://localhost:27017/todo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
