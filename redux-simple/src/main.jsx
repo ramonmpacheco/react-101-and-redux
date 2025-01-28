e6b32b3dade70e67c5f0755e36e89aacc63d640d
Ramon Pacheco <rmpacheco2@gmail.com>
2025-01-28 06:54:32 -0300

    add store

--- redux-simple/src/main.jsx
+++ redux-simple/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {Provider} from 'react-redux'
+import storeConfig from './store/storeConfig.js'
+
+const store = storeConfig()
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App/>
+    </StrictMode>
+  </Provider>
 )
