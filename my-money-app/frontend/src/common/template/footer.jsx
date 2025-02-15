5796f29d99b9fb2768b81cbbf614573225fb8028
Ramon Pacheco <rmpacheco2@gmail.com>
2025-02-15 15:05:44 -0300

    add front - wip

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2025
+      <a href='http://google.com' target='_blank'> R5</a>.
+    </strong>
+  </footer>
+)
