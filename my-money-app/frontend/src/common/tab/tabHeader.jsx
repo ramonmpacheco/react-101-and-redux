f113aacd33aa9c821be809141d27db24f0023145
Ramon Pacheco <rmpacheco2@gmail.com>
2025-02-20 06:39:26 -0300

    add front - wip

---
+++ my-money-app/frontend/src/common/tab/tabHeader.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {bindActionCreators} from "redux";
+import {selectTab} from "./tabActions";
+import {connect} from "react-redux";
+
+class TabHeader extends React.Component {
+  render() {
+    const selected = this.props.tab.selected === this.props.target
+    return (
+      <li className={selected ? "active" : ''}>
+        <a
+          data-toggle='tab'
+          href="javascript:"
+          data-target={this.props.target}
+          onClick={() => this.props.selectTab(this.props.target)}
+        >
+          <i className={`fa fa-${this.props.icon}`}/>
+          {this.props.label}
+        </a>
+      </li>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({tab: state.tab})
+const mapDispatchToProps = (dispatch) => bindActionCreators({selectTab}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader);
--- my-money-app/frontend/src/paymentCycle/paymentCycle.jsx
+++ my-money-app/frontend/src/paymentCycle/paymentCycle.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
+import TabContent from "../common/tab/tabContent";
 
-export default props => (
-  <div>
-    <h1>Ciclo Pagamento</h1>
-  </div>
-)
+class PaymentCycle extends React.Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Ciclos de Pagamento' small='Cadastro'/>
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label='Listar' icon='bars' target='tabList'/>
+              <TabHeader label='Incluir' icon='plus' target='tabCreate'/>
+              <TabHeader label='Alterar' icon='pencil' target='tabUpdate'/>
+              <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id='tabList'><h1>Lista</h1></TabContent>
+              <TabContent id='tabCreate'><h1>Create</h1></TabContent>
+              <TabContent id='tabUpdate'><h1>Update</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Delete</h1></TabContent>
+            </TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default PaymentCycle
